ade0d2a91250ab9a2673d6c025dbbec417f53900
lonnonjamesd <lemonjameslem@protonmail.com>
2022-03-26 23:19:06 -0400

    Skipping if None

--- src/ops/op2.rs
+++ src/ops/op2.rs
@@ -12,8 +12,11 @@ pub struct Op2 {
 pub struct Op2Data {
     pub token: String,
     pub properities: Properties,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub compress: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub large_threshold: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub shards: Option<Vec<i32>>,
     pub intents: i32,
 }
